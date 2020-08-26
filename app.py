d94c11dfe616bae41c8d6deb23a81bb3b4bf33b1
tyler <tduic@andrew.cmu.edu>
2020-08-25 20:24:31 -0400

    added framework for retrieving option chains and placing option orders

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from chalice import Chalice
+from constants import *
 from login import Login
 from tda import client
 
@@ -9,28 +10,69 @@ app = Chalice(app_name='AlgoTrading_Chalice')
 def index():
     return {'hello': 'world'}
 
-@app.route('/quote')
-def quote():
+@app.route('/quote/{symbol}')
+def quote(symbol):
     t = Login()
-    response = t.get_quote('AAPL').json()
+    response = t.get_quote(symbol).json()
     return response
 
-# The view function above will return {"hello": "world"}
-# whenever you make an HTTP GET request to '/'.
-#
-# Here are a few more examples:
-#
-# @app.route('/hello/{name}')
-# def hello_name(name):
-#    # '/hello/james' -> {"hello": "james"}
-#    return {'hello': name}
-#
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.current_request.json_body
-#     # We'll echo the json body back to the user in a 'user' key.
-#     return {'user': user_as_json}
-#
-# See the README documentation for more examples.
-#
+@app.route('/option/chain/{symbol}')
+def option_chain(symbol):
+    t = Login()
+    response = t.get_option_chain(symbol).json()
+    return response
+
+@app.route('option/order', methods=['POST'])
+def option_order(symbol):
+    webhook_message = app.current_request.json_body
+
+    if 'passphrase' not in webhook_message:
+        return {
+            'code': 'error',
+            'message': 'unauthorized, passphrase not present'
+        }
+    if webhook_message['passphrase'] != PASSPHRASE:
+        return {
+            'code': 'error',
+            'message': 'unauthorized, passphrase invalid'
+        }
+
+    order_spec = {
+        'complexOrderStrategy': 'NONE',
+        'orderType': 'LIMIT',
+        'session': 'NORMAL',
+        'price': '0.00',
+        'duration': 'DAY',
+        'orderStrategyType': 'SINGLE',
+        'orderLegCollection': {
+            'instruction': 'BUY_TO_OPEN',
+            'quantity': 1,
+            'instrument': {
+                'symbol': 'AAPL_082820C500.0',
+                'assetType': 'OPTION'
+            }
+        }
+    }
+    # 'price': webhook_message['price']
+    # 'quantity': webhook_message['quantity']
+    # 'symbol': webhook_message['symbol']
+    response = t.place_order(ACCOUNT_ID, order_spec)
+    return response
+
+# example option order spec JSON
+# {
+#     'complexOrderStrategy': 'NONE',
+#     'orderType': 'LIMIT',
+#     'session': 'NORMAL',
+#     'price': '6.45',
+#     'duration': 'DAY',
+#     'orderStrategyType': 'SINGLE',
+#     'orderLegCollection': {
+#         'instruction': 'BUY_TO_OPEN',
+#         'quantity': 0,
+#         'instrument': {
+#             'symbol': 'MSFT_061220C182.5',
+#             'assetType': 'OPTION'
+#         }
+#     }
+# }
